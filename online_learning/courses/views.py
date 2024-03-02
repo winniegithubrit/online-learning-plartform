0d2235d52dd0ed97053d1e2f6faff1be46729196
winniegithubrit <winnie.jomo@student.moringaschool.com>
2024-03-03 01:04:07 +0300

    added home template

--- online_learning/courses/views.py
+++ online_learning/courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.template import loader
+
+
+def home(request):
+  template = loader.get_template('courses/home.html')
+  return HttpResponse(template.render())
 
 # Create your views here.
